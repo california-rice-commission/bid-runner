24f7040af0930c005eaff02c84ef769660ef3915
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-02-08 11:29:26 -0800

    make the bat script smarter by detecting the version

--- run-app.bat
+++ run-app.bat
@@ -1,1 +1,15 @@
-"C:/Program Files/R/R-4.2.2/bin/x64/R.exe" -e "shiny::runApp('.', launch.browser = TRUE)"
+@echo off 
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`Rscript --version`) DO (
+SET var=%%F
+)
+
+SET R_VERSION_DETECTED=%var:~20,5%
+
+SET PART1="C:/Program Files/R/R-
+SET PART2=/bin/x64/R.exe" -e "shiny::runApp('.', launch.browser = TRUE)"
+
+SET run_cmd=%PART1%%R_VERSION_DETECTED%%PART2%
+
+%run_cmd%
+
